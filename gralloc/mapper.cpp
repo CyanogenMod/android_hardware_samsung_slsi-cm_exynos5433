CyanogenMod/android_hardware_samsung_slsi-cm_exynos7420

gralloc: Improve map/unmap logging
Change-Id: I4ffee430d69a0e64cc3adf6bf6fb986417b9f9f9

gralloc/mapper.cpp
@@ -61,6 +61,7 @@ static int gralloc_map(gralloc_module_t const* module, buffer_handle_t handle)
         chroma_size = (hnd->vstride / 2) * ALIGN(hnd->stride / 2, 16) + ext_size;
         break;
     default:
+        ALOGE("%s: unknown format: 0x%x", __func__, hnd->format);
         break;
     }
 
@@ -110,6 +111,7 @@ static int gralloc_unmap(gralloc_module_t const* module, buffer_handle_t handle)
         chroma_size = (hnd->vstride / 2) * ALIGN(hnd->stride / 2, 16) + ext_size;
         break;
     default:
+        ALOGE("%s: unknown format: 0x%x", __func__, hnd->format);
         break;
     }
 

CyanogenMod/android_hardware_samsung_slsi-cm_exynos5433

gralloc/mapper.cpp
@@ -61,6 +61,7 @@ static int gralloc_map(gralloc_module_t const* module, buffer_handle_t handle)
         chroma_size = (hnd->vstride / 2) * ALIGN(hnd->stride / 2, 16) + ext_size;
         break;
     default:
+        ALOGV("%s: unknown format: 0x%x", __func__, hnd->format);
         break;
     }
 
@@ -110,6 +111,7 @@ static int gralloc_unmap(gralloc_module_t const* module, buffer_handle_t handle)
         chroma_size = (hnd->vstride / 2) * ALIGN(hnd->stride / 2, 16) + ext_size;
         break;
     default:
+        ALOGV("%s: unknown format: 0x%x", __func__, hnd->format);
         break;
     }
 